f7923ede376f5aa7a266dba05c7f2b915fc901a4
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2007-11-07 17:01:57 +0000

    Refactored texture.c
    git-svn-id: https://starruby.googlecode.com/svn/trunk@185 425b54fa-293c-0410-bdda-a7e424c12f17

--- texture.c
+++ texture.c
@@ -1,7 +1,9 @@
 #include "starruby.h"
 
 #define DIV255(x) ((x + 255) >> 8)
-#define ALPHA(src, dst, a) (DIV255((dst << 8) - dst + (src - dst) * a))
+#define ALPHA(src, dst, a) (a == 255 ? src :\
+                            (a == 0 ? dst :\
+                             DIV255((dst << 8) - dst + (src - dst) * a)))
 
 static VALUE symbol_add;
 static VALUE symbol_alpha;

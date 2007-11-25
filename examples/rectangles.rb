d8b6d11c5444800d5035d5e9ffa306cfbf45e580
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2007-11-25 17:42:19 +0000

    Modified examples/rectangles.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@305 425b54fa-293c-0410-bdda-a7e424c12f17

--- examples/rectangles.rb
+++ examples/rectangles.rb
@@ -16,9 +16,9 @@ Game.run(320, 240) do
     x2 = rand(320)
     y1 = rand(240)
     y2 = rand(240)
-    width  = (x1 - x2).abs
-    height = (y1 - y2).abs
-  end while width <= 0 or height <= 0
+  end while x1 == x2 or y1 == y2
+  width  = (x1 - x2).abs
+  height = (y1 - y2).abs
   x = [x1, x2].min
   y = [y1, y2].min
   color = Color.new(rand(256), rand(256), rand(256), rand(256))

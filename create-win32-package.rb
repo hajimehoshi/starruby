84fe138816bab72385e852a62e2a989d2ae31469
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2007-11-28 19:29:07 +0000

    Added README
    git-svn-id: https://starruby.googlecode.com/svn/trunk@316 425b54fa-293c-0410-bdda-a7e424c12f17

--- create-win32-package.rb
+++ create-win32-package.rb
@@ -5,13 +5,21 @@ include FileUtils
 
 def main
   show_usage if ARGV.length != 0
-  version = "0.0.0"
-  open("win32/readme.txt") do |fp|
-    version = fp.gets[/\d+\.\d+\.\d+/]
+  title = ""
+  open("README") do |fp|
+    title = fp.gets.chomp
+  end
+  version = title[/\d+\.\d+\.\d+/]
+  open("win32/readme.txt.tmpl", "r") do |fp|
+    new_text = fp.read.sub("%title%", title)
+    open("win32/readme.txt", "w") do |fp2|
+      fp2.write(new_text)
+    end
   end
   main_dir = "starruby-#{version}-win32"
   mkdir_p(main_dir, :verbose => true)
   Dir["win32/**/*"].each do |path|
+    next if path =~ /tmpl$/
     next unless FileTest.file?(path)
     dir = File.join(main_dir, File.dirname(path[6..-1]))
     unless FileTest.directory?(dir)

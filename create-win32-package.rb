03d05dc02bb4a3f8e1ddb296b2e362a508ef098a
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2007-11-28 19:32:23 +0000

    Modified create-win32-package.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@317 425b54fa-293c-0410-bdda-a7e424c12f17

--- create-win32-package.rb
+++ create-win32-package.rb
@@ -10,14 +10,13 @@ def main
     title = fp.gets.chomp
   end
   version = title[/\d+\.\d+\.\d+/]
-  open("win32/readme.txt.tmpl", "r") do |fp|
-    new_text = fp.read.sub("%title%", title)
-    open("win32/readme.txt", "w") do |fp2|
-      fp2.write(new_text)
-    end
-  end
   main_dir = "starruby-#{version}-win32"
   mkdir_p(main_dir, :verbose => true)
+  open("win32/readme.txt.tmpl", "r") do |fp|
+    open(File.join(main_dir, "readme.txt"), "w") do |fp2|
+      fp2.write(fp.read.sub("%title%", title))
+    end
+  end
   Dir["win32/**/*"].each do |path|
     next if path =~ /tmpl$/
     next unless FileTest.file?(path)
@@ -35,8 +34,8 @@ def main
     end
     cp(path, dir, :verbose => true)
   end
-  mkdir(File.join(main_dir, "ext"))
-  cp("starruby.so", File.join(main_dir, "ext"))
+  mkdir(File.join(main_dir, "ext"), :verbose => true)
+  cp("starruby.so", File.join(main_dir, "ext"), :verbose => true)
 end
 
 def show_usage

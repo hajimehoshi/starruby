1e6198b8a36ec580dc7852b0c532ecda34a4175a
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-01-13 03:19:17 +0000

    Modified perspective.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@455 425b54fa-293c-0410-bdda-a7e424c12f17

--- samples/perspective.rb
+++ samples/perspective.rb
@@ -11,10 +11,11 @@ camera.y = texture.height / 2
 camera.height = 40
 camera.angle_n = 0
 
-viewport = Texture.new(160, 120)
+screen = Texture.new(160, 120)
 
 font = Font.new("fonts/ORANGEKI", 12)
 white = Color.new(255, 255, 255)
+distance = 60
 
 Game.title = "Perspective"
 
@@ -42,17 +43,22 @@ Game.run(320, 240, :window_scale => 2) do
   elsif keys.include?(:s)
     camera.angle_n = (camera.angle_n + 1) % 64
   end
+  if keys.include?(:r)
+    distance += 1
+  elsif keys.include?(:c)
+    distance -= 1
+  end
   
   angle = camera.angle_n * 2 * Math::PI / 64
-  viewport.clear
-  viewport.render_in_perspective(texture, camera.x, camera.y, camera.height, angle, 60)
+  screen.clear
+  screen.render_in_perspective(texture, camera.x, camera.y, camera.height, angle, distance)
   
   s = Game.screen
   s.clear
-  s.render_texture(viewport, 0, 0)
-  s.render_text("(x,y,height,angle) =" % angle, 8, viewport.height + 8, font, white)
+  s.render_texture(screen, 0, 0)
+  s.render_text("(x,y,height,angle) =" % angle, 8, screen.height + 8, font, white)
   str = "(#{camera.x},#{camera.y},#{camera.height},%0.4f)" % angle
-  s.render_text(str, 8, viewport.height + 24, font, white)
+  s.render_text(str, 8, screen.height + 24, font, white)
   x = s.width  / 2 + 10
   y = s.height / 2 + 10
   s.render_texture(texture, x, y, :scale_x => 0.5, :scale_y => 0.5)
--- test/benchmark.rb
+++ test/benchmark.rb
@@ -5,18 +5,18 @@ require "starruby"
 
 include StarRuby
 
-texture = Texture.load("images/ruby")
-texture2 = Texture.new(100, 100)
+texture1 = Texture.new(1000, 1000)
+texture2 = Texture.new(1000, 1000)
 
 Benchmark.bm do |x|
   x.report("normal") do
-    2000.times do |i|
-      texture2.render_texture(texture, i % 13, i % 17)
+    100.times do |i|
+      texture2.render_texture(texture1, i % 13, i % 17)
     end
   end
   x.report("perspective") do
-    2000.times do |i|
-      texture2.render_in_perspective(texture, i % 13, i % 17, i % 19 + 1, i % 23, i % 27)
+    100.times do |i|
+      texture2.render_in_perspective(texture1, 500, 500, i % 19 + 10, i % 23, i % 27)
     end
   end
 end

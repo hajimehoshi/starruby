5761e0fab9799b96783c3987694abaf7ab678952
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-01-22 13:10:36 +0000

    Modified extconf.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@493 425b54fa-293c-0410-bdda-a7e424c12f17

--- extconf.rb
+++ extconf.rb
@@ -19,7 +19,10 @@ have_library("SDL_ttf") or exit(false)
 have_header("png.h") or exit(false)
 have_header("zlib.h") or exit(false)
 
-have_header("fontconfig/fontconfig.h") and have_library("fontconfig", "FcInit")
+if CONFIG["arch"] =~ /linux|darwin/
+  have_header("fontconfig/fontconfig.h") or exit(false)
+  have_library("fontconfig", "FcInit") or exit(false)
+end
 
 if arg_config("--debug", false)
   $CFLAGS += " -DDEBUG -O0 -g"

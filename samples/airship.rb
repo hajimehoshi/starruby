ff8abafbca85c3aa1732efead73c492496a42ecb
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-01-25 08:22:51 +0000

    Modified airship.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@504 425b54fa-293c-0410-bdda-a7e424c12f17

--- samples/airship.rb
+++ samples/airship.rb
@@ -13,6 +13,10 @@ airship.yaw_int    = 0
 airship.pitch_int  = 0
 airship.roll_int   = 0
 airship.height     = 25
+fearless = false
+
+font = Font.new("fonts/ORANGEKI", 12)
+yellow = Color.new(255, 255, 128)
 
 class Numeric
   def to_radian
@@ -22,19 +26,23 @@ end
 
 Game.title = "Airship"
 Game.run(320, 240) do
+  if Input.keys(:keyboard, :duration => 1).include?(:f)
+    fearless = ! fearless
+  end
   keys = Input.keys(:keyboard)
   Game.terminate if keys.include?(:escape)
   if keys.include?(:left)
     airship.yaw_int = (airship.yaw_int + 127) % 128
-    airship.roll_int = [airship.roll_int - 1, -48].max
+    
+    airship.roll_int = [airship.roll_int - (fearless ? 4 : 1), fearless ? -64 : -8].max
   elsif keys.include?(:right)
     airship.yaw_int = (airship.yaw_int + 1) % 128
-    airship.roll_int = [airship.roll_int + 1, 48].min
+    airship.roll_int = [airship.roll_int + (fearless ? 4 : 1), fearless ? 64 : 8].min
   else
     if 0 < airship.roll_int
-      airship.roll_int -= 1
+      airship.roll_int -= (fearless ? 4 : 1)
     elsif airship.roll_int < 0
-      airship.roll_int += 1
+      airship.roll_int += (fearless ? 4 : 1)
     end
   end
   yaw_radian = airship.yaw_int.to_radian
@@ -43,16 +51,16 @@ Game.run(320, 240) do
     airship.y = (airship.y - 10 * Math.cos(yaw_radian)).to_i
   end
   if keys.include?(:down)
-    airship.height = [airship.height + 2, 100].min
-    airship.pitch_int = [airship.pitch_int + 1, 48].min
+    airship.height = [airship.height + 2, 45].min
+    airship.pitch_int = [airship.pitch_int + (fearless ? 4 : 1), fearless ? 64 : 8].min
   elsif keys.include?(:up)
-    airship.height = [airship.height - 2, -100].max
-    airship.pitch_int = [airship.pitch_int - 1, -48].max
+    airship.height = [airship.height - 2, 5].max
+    airship.pitch_int = [airship.pitch_int - (fearless ? 4 : 1), fearless ? -64 : -8].max
   else
     if 0 < airship.pitch_int
-      airship.pitch_int -= 1
+      airship.pitch_int -= (fearless ? 4 : 1)
     elsif airship.pitch_int < 0
-      airship.pitch_int += 1
+      airship.pitch_int += (fearless ? 4 : 1)
     end
   end
   s = Game.screen
@@ -72,4 +80,8 @@ Game.run(320, 240) do
     :loop => true,
   }
   s.render_in_perspective(field_texture, options)
+  s.render_text("[Arrow] Rotate Camera", 8, 8, font, yellow)
+  s.render_text("[Space] Go Forward", 8, 8 + 16, font, yellow)
+  str = "[F] Fearless? #{fearless ? '(Yes)' : '(No)'}"
+  s.render_text(str, 8, 8 + 32, font, yellow)
 end

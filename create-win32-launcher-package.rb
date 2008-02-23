65e691835248f8fccfdf41294d3082896df442cc
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-02-23 14:13:23 +0000

    Updated launcher
    git-svn-id: https://starruby.googlecode.com/svn/trunk@602 425b54fa-293c-0410-bdda-a7e424c12f17

--- create-win32-launcher-package.rb
+++ create-win32-launcher-package.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require "fileutils"
+require "rbconfig"
+
 include FileUtils
 
 def main
@@ -20,13 +22,29 @@ def main
   Dir["win32/dll/*.dll"].each do |path|
     cp(path, main_dir, :verbose => true)
   end
+  # samples
   Dir["samples/**/*"].each do |path|
     next unless FileTest.file?(path)
     dir = File.join(main_dir, File.dirname(path))
     mkdir_p(dir, :verbose => true) unless FileTest.directory?(dir)
     cp(path, dir, :verbose => true)
   end
+  # star ruby so
   cp("starruby.so", main_dir, :verbose => true)
+  # launcher exe
+  cd("win32/launcher") do
+    system("c:\\ruby\\bin\\mkexy.bat", "starruby-launcher.rb")
+    system("c:\\ruby\\bin\\exerb.bat", "starruby-launcher.exy", "-cguirt", "-kUTF8")
+  end
+  cp("win32/launcher/starruby-launcher.exe", main_dir, :verbose => true)
+  path = File.join(Config::CONFIG["prefix"], "share/exerb/*.dll")
+  exerb_dll = Dir[path][-1]
+  # exerb dll
+  cp(exerb_dll, main_dir, :verbose => true)
+  # ruby libraries
+  lib_dir = File.join(main_dir, "lib")
+  mkdir(lib_dir, :verbose => true) unless FileTest.directory?(lib_dir)
+  cp_r(RbConfig::CONFIG["rubylibdir"], lib_dir, :verbose => true)
 end
 
 def show_usage
--- win32/install.rb
+++ win32/install.rb
@@ -11,12 +11,12 @@ sitearchdir = Config::CONFIG["sitearchdir"]
 FileUtils.mkdir_p(dlldir)
 FileUtils.mkdir_p(sitearchdir)
 
-Dir.glob("dll/*.dll") do |path|
+Dir["dll/*.dll"] do |path|
   if path =~ /zlib/
     next if File.exist?(File.join(dlldir, File.basename(path)))
   end
   FileUtils.install(path, dlldir, option)
 end
-Dir.glob("ext/*.so")  {|path| FileUtils.install(path, sitearchdir, option)}
+Dir["ext/*.so"] {|path| FileUtils.install(path, sitearchdir, option)}
 
 puts "Installation Star Ruby completed!"

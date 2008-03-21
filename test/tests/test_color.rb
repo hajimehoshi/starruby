1f781f0a5e1a246920b2e50f69d119477deccdf4
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-03-21 18:08:22 +0000

    Added Color.[]
    git-svn-id: https://starruby.googlecode.com/svn/trunk@651 425b54fa-293c-0410-bdda-a7e424c12f17

--- test/tests/test_color.rb
+++ test/tests/test_color.rb
@@ -25,6 +25,26 @@ class TestColor < Test::Unit::TestCase
     assert_equal c2.hash, Color.new(5, 6, 7).hash
   end
 
+  def test_color
+    c1 = Color[1, 2, 3, 4]
+    c2 = Color[5, 6, 7]
+
+    assert_equal 1, c1.red
+    assert_equal 2, c1.green
+    assert_equal 3, c1.blue
+    assert_equal 4, c1.alpha
+    assert_equal 255, c2.alpha
+
+    assert c1 == Color.new(1, 2, 3, 4)
+    assert c2 == Color.new(5, 6, 7)
+    assert c2 == Color.new(5, 6, 7, 255)
+    assert c1 != Color.new(1, 2, 3, 5)
+    assert c1 != Object.new
+    assert c1.eql?(Color.new(1, 2, 3, 4))
+    assert_equal c1.hash, Color.new(1, 2, 3, 4).hash
+    assert_equal c2.hash, Color.new(5, 6, 7).hash
+  end
+
   def test_color_type
     assert_raise TypeError do
       Color.new(nil, 2, 3, 4)

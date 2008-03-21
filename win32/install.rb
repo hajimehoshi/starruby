59f32efa8366e1db0c2ba9c8f09a2c69e695111e
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-03-21 11:04:37 +0000

    Modified install.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@650 425b54fa-293c-0410-bdda-a7e424c12f17

--- win32/install.rb
+++ win32/install.rb
@@ -11,12 +11,12 @@ sitearchdir = Config::CONFIG["sitearchdir"]
 FileUtils.mkdir_p(dlldir)
 FileUtils.mkdir_p(sitearchdir)
 
-Dir["dll/*.dll"] do |path|
+Dir.glob("dll/*.dll") do |path|
   if path =~ /zlib/
     next if File.exist?(File.join(dlldir, File.basename(path)))
   end
   FileUtils.install(path, dlldir, option)
 end
-Dir["ext/*.so"] {|path| FileUtils.install(path, sitearchdir, option)}
+Dir.glob("ext/*.so") {|path| FileUtils.install(path, sitearchdir, option)}
 
 puts "Installation Star Ruby completed!"

53ab64211fe9c19241a09cb0ba3bfc18c8b97bda
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-07-11 00:05:38 +0000

    Updated version
    git-svn-id: https://starruby.googlecode.com/svn/trunk@844 425b54fa-293c-0410-bdda-a7e424c12f17

--- test/tests/test_star_ruby.rb
+++ test/tests/test_star_ruby.rb
@@ -5,7 +5,7 @@ require "starruby"
 class TestStarRuby < Test::Unit::TestCase
 
   def test_version
-    assert_equal "0.2.0", StarRuby::VERSION
+    assert_equal "0.3.0", StarRuby::VERSION
     assert StarRuby::VERSION.frozen?
   end
 

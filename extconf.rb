cb171415ed118b01e343b5bf1d17cf80c884e6a3
hajimehoshi <hajimehoshi@425b54fa-293c-0410-bdda-a7e424c12f17>
2008-11-15 17:56:27 +0000

    Modified extconf.rb
    git-svn-id: https://starruby.googlecode.com/svn/trunk@895 425b54fa-293c-0410-bdda-a7e424c12f17

--- extconf.rb
+++ extconf.rb
@@ -1,39 +1,5 @@
 require "mkmf"
 
-if arg_config("--gp2x", false)
-=begin
-  case CONFIG["arch"]
-  when /darwin/
-    # http://wiki.gp2x.org/wiki/Setting_up_a_development_environment_%28Mac%29
-    sdl_base      = "/opt/local/gp2x/bin/arm-open2x-linux-" # todo
-    libpng_base   = "" # todo
-    cross_compile = "/opt/local/devkitpro/devkitGP2X/bin/arm-linux-"
-  when /linux/
-    sdl_base      = "/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/"
-    libpng_base   = "/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/"
-    cross_compile = "/opt/open2x/gcc-4.1.1-glibc-2.3.6/bin/arm-open2x-linux-"
-  else
-    raise "not supported arch: #{CONFIG["arch"]}"
-  end
-  CONFIG["CC"]    = "#{cross_compile}gcc"
-  CONFIG["STRIP"] = "#{cross_compile}strip"
-
-  $CFLAGS  += " " + `#{sdl_base}sdl-config --cflags`.chomp
-  $CFLAGS  += " " + `#{libpng_base}libpng-config --cflags`.chomp
-  $LDFLAGS += " " + `#{sdl_base}sdl-config --libs`.chomp
-  $LDFLAGS += " " + `#{libpng_base}libpng-config --ldflags`.chomp
-  $CFLAGS += " -finline-functions -Wall -std=c99-funit-at-a-time"
-
-  $libs += " -lSDL_mixer -lSDL_ttf"
-  have_header("png.h") or exit(false)
-  have_header("zlib.h") or exit(false)
-
-  create_makefile("starruby")
-  exit
-=end
-  $CFLAGS += " -DGP2X"
-end
-
 case CONFIG["arch"]
 when /mingw32/
   $CFLAGS += " -DWIN32"
@@ -57,8 +23,12 @@ if CONFIG["arch"] =~ /linux|darwin/
 end
 
 $CFLAGS += " -finline-functions -Wall -W -Wpointer-arith -Wno-unused-parameter -pedantic -std=c99 -funit-at-a-time"
-$CFLAGS += " -mfpmath=sse -msse2" if RUBY_PLATFORM !~ /^powerpc/ and CONFIG["arch"] !~ /darwin/
-$LDFLAGS += " -Wl,--no-undefined" if CONFIG["arch"] !~ /darwin/
+if RUBY_PLATFORM !~ /^powerpc/ and CONFIG["arch"] !~ /darwin/
+  $CFLAGS += " -mfpmath=sse -msse2"
+end
+if CONFIG["arch"] !~ /darwin/
+  $LDFLAGS += " -Wl,--no-undefined"
+end
 
 if arg_config("--debug", false)
   $CFLAGS += " -DDEBUG -O0 -g -pg"

eeb71bd97d4814c5b5535e48d2acdbb07d11d07c
Hajime Hoshi <hajimehoshi@gmail.com>
2010-09-25 01:09:04 +0900

    bug fix: #3 Font#exist?

--- src/font.c
+++ src/font.c
@@ -45,7 +45,11 @@ SearchFont(VALUE rbFilePathOrName,
     return;
   }
   volatile VALUE rbFontNameSymbol =
+#ifdef rb_intern_str
+    ID2SYM(rb_intern_str(rbFilePathOrName));
+#else
     ID2SYM(rb_intern(StringValueCStr(rbFilePathOrName)));
+#endif
   FontFileInfo* info = fontFileInfos;
   while (info) {
     if (info->rbFontNameSymbol == rbFontNameSymbol) {
